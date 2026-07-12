9bd37c3737fb0fefc15c6593ac7d45d077126d86
Lukas Hoehl <lukas.hoehl@stackit.cloud>
2026-07-12 18:37:05 +0200

    Revert "dummy xdp prog for veth of lb"
    This reverts commit a39ce7670a80b090a1258c297aee3e502d308f26.

--- test/dummyxdp/gen.go
+++
@@ -1,3 +0,0 @@
-package dummyxdp
-
-//go:generate  go tool bpf2go -cc clang-22 -tags linux dummy prog.c -- -I../../internal/bpf/kern/include/
--- test/dummyxdp/prog.c
+++
@@ -1,8 +0,0 @@
-#include "vmlinux.h"
-#include <bpf/bpf_endian.h>
-#include <bpf/bpf_helpers.h>
-
-SEC("xdp")
-int dummy(struct xdp_md *ctx) { return XDP_PASS; }
-
-char __license[] SEC("license") = "Dual MIT/GPL";
