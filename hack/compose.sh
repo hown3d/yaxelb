9c03b54b574932effc0cca66cabfafdb3f6af317
Lukas Hoehl <lukas.hoehl.316@gmail.com>
2026-07-14 16:54:47 +0200

    print veth configuration in compose script

--- hack/compose.sh
+++ hack/compose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function run_ethtool() {
-  docker run --network=host --privileged -v /sys:/sys nicolaka/netshoot ethtool -K "$("$DIR"/tools/veth-of-container.sh yaxelb-lb-1)" "$@"
+  docker run --network=host --privileged -v /sys:/sys nicolaka/netshoot ethtool -K "$@"
 }
 
 set -eo pipefail
@@ -10,8 +10,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && /bin/pwd)"
 
 docker compose "$1" --build --detach
 
-# configure veth to support XDP_REDIRECT
-run_ethtool gro on
-run_ethtool tx-checksumming off
+veth="$("$DIR"/tools/veth-of-container.sh yaxelb-lb-1)"
+echo "configure $veth to support XDP_REDIRECT"
+run_ethtool "$veth" gro on
+run_ethtool "$veth" tx-checksumming off
 
 docker compose logs -f
